b4117ea6491c9d3a1c86d61c53bf870a6bdf6e1e
Chinonso Mogo <mogononso@gmail.com>
2023-05-22 10:30:48 -0230

    added documentation for transform function

--- transform_data_to_df.py
+++ transform_data_to_df.py
@@ -3,28 +3,39 @@ import ingest_data_to_json as ingest
 import asyncio
 
 
+def transform(file) -> pd.DataFrame:
+    """
+    Accepts a json file as argument and transform it into a pandas dataframe.
+    
+    Parameters:
+    argument (json): JSON file
+    
+    Returns:
+    DataFrame: A pandas dataframe
+    """
+    # df = pd.read_json("mls_listing.json")
+    df = pd.read_json(file)
 
-# df = pd.read_json("mls_listing.json")
+    # DATA TRANSFORMATION
+    # ================================================================================================================
 
-# # DATA TRANSFORMATION
-# # ================================================================================================================
+    df[['mls_escape','mls_num']] = df.mls_num.str.split(":", expand=True)
+    df[['city', 'province', 'postal_code']] = df.city_postal_code.str.split(",", expand=True)
+    df['last_updated'] = df.last_updated.str[4:]
+    df.drop(columns=['city_postal_code', 'mls_escape',], axis=1, inplace=True)
+    df['price'] = df.price.str[1:]
+    df['sqft'] = df['sqft'].str.replace("SQFT", "")
+    df['last_updated'] = df['last_updated'].str.replace(',', '').str.strip().str.replace(" ", "-")
+    df['last_updated'] = pd.to_datetime(df['last_updated'], format="%B-%d-%Y")
+    df['price'] = df['price'].str.replace(',', '')
+    df['city'] = df['city'].str.title()
+    df['city'] = df['city'].str.replace("'S", "'s")
+    df['address_street'] = df['address_street'].str.title()
+    df['postal_code'] = df['postal_code'].str.upper()
+    df['sqft'] = df['sqft'].str.replace(',', '')
+    df[['sqft', 'num_of_beds']] = df[['sqft', 'num_of_beds']].replace('N/A', '0')
+    df[['num_of_baths', 'num_of_beds', 'sqft', 'price']] = df[['num_of_baths', 'num_of_beds', 'sqft', 'price']].astype(int)
 
-# df[['mls_escape','mls_num']] = df.mls_num.str.split(":", expand=True)
-# df[['city', 'province', 'postal_code']] = df.city_postal_code.str.split(",", expand=True)
-# df['last_updated'] = df.last_updated.str[4:]
-# df.drop(columns=['city_postal_code', 'mls_escape',], axis=1, inplace=True)
-# df['price'] = df.price.str[1:]
-# df['sqft'] = df['sqft'].str.replace("SQFT", "")
-# df['last_updated'] = df['last_updated'].str.replace(',', '').str.strip().str.replace(" ", "-")
-# df['last_updated'] = pd.to_datetime(df['last_updated'], format="%B-%d-%Y")
-# df['price'] = df['price'].str.replace(',', '')
-# df['city'] = df['city'].str.title()
-# df['city'] = df['city'].str.replace("'S", "'s")
-# df['address_street'] = df['address_street'].str.title()
-# df['postal_code'] = df['postal_code'].str.upper()
-# df['sqft'] = df['sqft'].str.replace(',', '')
-# df[['sqft', 'num_of_beds']] = df[['sqft', 'num_of_beds']].replace('N/A', '0')
-# df[['num_of_baths', 'num_of_beds', 'sqft', 'price']] = df[['num_of_baths', 'num_of_beds', 'sqft', 'price']].astype(int)
+    return df
 
-
-asyncio.run(ingest.main())
+# asyncio.run(ingest.main())
